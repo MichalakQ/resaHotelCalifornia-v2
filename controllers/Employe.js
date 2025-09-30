dc82938895169a1d40d8c43324cb53a3d67274f1
MichalakQ <q.michalak@laposte.net>
2025-09-30 14:37:28 +0200

    actualisation employes

--- controllers/Employe.js
+++ controllers/Employe.js
@@ -3,38 +3,38 @@ const Chambre = require('../models/Employe');
 module.exports = {
   // Liste
   async index(req, res) {
-    const client= await Client.getAll();
-    res.render('client/index', { client });
+    const employe= await Employe.getAll();
+    res.render('employe/index', { employe });
   },
 
   // Formulaire de création
   createForm(req, res) {
-    res.render('client/create');
+    res.render('employe/create');
   },
 
   // Création
   async create(req, res) {
-    const { numero, capacite } = req.body;
-    await Client.create({ numero, capacite });
-    res.redirect('/client');
+    const { nom, email, telephone, nombre_personnes } = req.body;
+    await Client.create({ nom, email, telephone, nombre_personnes });
+    res.redirect('/employe');
   },
 
   // Formulaire d’édition
   async editForm(req, res) {
-    const client = await Client.getById(req.params.id);
-    res.render('client/edit', { chambre });
+    const employe = await Employe.getById(req.params.id);
+    res.render('employe/edit', { employe });
   },
 
   // Modification
   async update(req, res) {
-    const { numero, capacite } = req.body;
-    await Client.update(req.params.id, { numero, capacite });
-    res.redirect('/client');
+    const {nom, email, telephone, nombre_personnes} = req.body;
+    await Client.update(req.params.id, {nom, email, telephone, nombre_personnes});
+    res.redirect('/employe');
   },
 
   // Suppression
   async remove(req, res) {
     await Client.remove(req.params.id);
-    res.redirect('/client');
+    res.redirect('/employe');
   }
 };
