703fd1413446c7019cfaa8ad6bcc05538f2fad39
adam <adam@me.fr>
2025-10-01 11:08:10 +0200

    creation et finalisation du CRUD Reservation

--- models/Reservation.js
+++ models/Reservation.js
@@ -11,85 +11,86 @@ class Reservation {
     static async findAll() {
         try {
             const [rows] = await db.execute('SELECT * FROM reservations ORDER BY chambre_id, client_id');
-            return rows.map(row => new Chambre(row));
+            return rows.map(row => new Reservation(row));
         } catch (error) {
             throw new Error('Erreur lors de la récupération des réservations: ' + error.message);
         }
     }
-    // Récupérer une client par ID
+    // Récupérer une reservation par ID
     static async findById(id) {
         try {
-            const [rows] = await db.execute('SELECT * FROM clients WHERE id = ?', [id]);
-            return rows.length > 0 ? new Client(rows[0]) : null;
+            const [rows] = await db.execute('SELECT * FROM reservation WHERE id = ?  AND autre_id = ?', [id, secondId]);
+            return rows.length > 0 ? new Reservation(rows[0]) : null;
         } catch (error) {
             throw new Error('Erreur lors de la récupération de la client: ' + error.message);
         }
     }
-    // Créer un nouveaux client
-    static async create(clientData) {
+    // Créer une nouvelle reservation
+    static async create(ReservationData) {
         try {
             const [result] = await db.execute(
-                'INSERT INTO client (nom, email, telephone, nombre_personnes) VALUES (?, ?, ?, ?)',
-                [clientData.nom, clientData.email, clientData.telephone, clientData.nombre_personnes]
+                'INSERT INTO reservation (client_id, chambre_id, date_arrivee, date_depart) VALUES (?, ?, ?, ?)',
+                [ReservationData.client_id, ReservationData.chambre_id, ReservationData.date_arrivee, ReservationData.date_depart]
             );
             return result.insertId;
         } catch (error) {
             if (error.code === 'ER_DUP_ENTRY') {
-                throw new Error('Ce client existe déjà');
+                throw new Error('Cette reservation existe déjà');
             }
-            throw new Error('Erreur lors de la création du client: ' + error.message);
+            throw new Error('Erreur lors de la création de la reservation: ' + error.message);
         }
     }
-    // Mettre à jour une chambre
-    async update(clientData) {
+    // Mettre à jour une reservation
+    async update(ReservationData) {
         try {
             await db.execute(
-                'UPDATE client SET nom = ?, email = ?, telephone = ?, nombre_personnes = ?,  WHERE id = ?',
-                [clientData.nom, clientData.email, clientData.telephone, clientData.nombre_personnes, this.id]
+                'UPDATE reservation SET client_id = ?, chambre_id = ?, date_arrivee = ?, date_depart = ?,  WHERE id = ?',
+                [ReservationData.client_id, ReservationData.chambre_id, ReservationData.date_arrivee, ReservationData.date_depart, this.id]
             );
-            this.nom = clientData.nom;
-            this.email = clientData.email;
-            this.telephone = clientData.telephone;
-            this.nombre_personnes = clientData.nombre_personnes;
+            
+            this.clients_id = ReservationData.client_id;
+            this.chambre_id = ReservationData.chambre_id;
+            this.date_arrivee = ReservationData.date_arrivee;
+            this.date_depart = ReservationData.date_depart;
             return true;
         } catch (error) {
             if (error.code === 'ER_DUP_ENTRY') {
-                throw new Error('Ce client existe déjà');
+                throw new Error('Cette reservation existe déjà');
             }
-            throw new Error('Erreur lors de la mise à jour du client: ' + error.message);
+            throw new Error('Erreur lors de la mise à jour de la reservation: ' + error.message);
         }
     }
-    // Supprimer un client
+    // Supprimer une reservation
     async delete(id) {
         try {
-            // Vérifier s'il y a des noms de clients existant
+            // Vérifier s'il y a des reservations existantes
             
-            const [clients] = await db.execute(
-                'SELECT COUNT(*) as count FROM client WHERE client_id = ?',
-                [id]
+            const [reservations] = await db.execute(
+                'SELECT COUNT(*) as count FROM reservation WHERE client_id = ?  AND chambre_id = ?',
+                [id, secondeId]
             );
-            if (clients[0].count > 0) {
-                throw new Error('Impossible de supprimer le nom des clients sont associées');
+            if (reservations[0].count > 0) {
+                throw new Error('Impossible de supprimer la reservation sont associées');
             }
-            await db.execute('DELETE FROM clients WHERE id = ?', [id]);
+            await db.execute('DELETE FROM reservation WHERE id = ? AND chambre_id = ?', [id, secondeId]);
             return true;
         } catch (error) {
-            throw new Error('Erreur lors de la suppression du client: ' + error.message);
+            throw new Error('Erreur lors de la suppression de la reservation: ' + error.message);
         }
     }
-    // Vérifier l'existance d'un client
-    static async isAvailable(clientsId, email, telephone, nombre_personnes) {
+    // Vérifier l'existance d'une reservation
+    static async isAvailable(clients_id, chambre_id, date_arrivee, date_depart) {
         try {
             const [rows] = await db.execute(`
     SELECT COUNT(*) as count 
-    FROM clients
-    WHERE clients_id = ?
+    FROM reservation
+    WHERE clients_id = ? chambre_id = ?'
     
-    `, [clientsId]);
+    `, [clients_id, chambre_id]);
             return rows[0].count === 0;
         } catch (error) {
-            throw new Error("Erreur lors de la vérification de l'existance du client: " + error.message);
+            throw new Error("Erreur lors de la vérification de l'existance d'une reservation: " + error.message);
         }
     }
 }
-module.exports = Client;
+module.exports = Reservation;
