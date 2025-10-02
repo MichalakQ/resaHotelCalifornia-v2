58d8b1c792e5a1f1115eef5896e248c557ae0933
MichalakQ <q.michalak@laposte.net>
2025-10-02 15:13:55 +0200

    control chambre

--- routes/chambres.js
+++ routes/chambres.js
@@ -1,49 +1,55 @@
-// Import d’Express pour créer un routeur
-const express = require('express');
+// Import d'Express pour créer un routeur
+import express from 'express';
 const router = express.Router();
 
 // Import du contrôleur qui contient la logique métier
-const chambreController = require('../controllers/chambreController');
+import ChambreController from '../controllers/chambreController.js';
 
 /**
  * Route : GET /chambres
  * Description : Affiche la liste des chambres
- * et le lien vers le formulaire d’ajout
+ * et le lien vers le formulaire d'ajout
  */
-router.get('/', chambreController.index);
+router.get('/', ChambreController.index);
 
 /**
  * Route : GET /chambres/create
- * Description : Affiche le formulaire de création d’une chambre
+ * Description : Affiche le formulaire de création d'une chambre
  */
-router.get('/create', chambreController.createForm);
+router.get('/create', ChambreController.create);
 
 /**
  * Route : POST /chambres/create
  * Description : Récupère les données envoyées par le formulaire
  * et ajoute une nouvelle chambre en base
  */
-router.post('/create', chambreController.create);
+router.post('/create', ChambreController.store);
 
 /**
  * Route : GET /chambres/:id/edit
- * Description : Affiche le formulaire d’édition d’une chambre existante
- * :id correspond à l’identifiant de la chambre dans l’URL
+ * Description : Affiche le formulaire d'édition d'une chambre existante
+ * :id correspond à l'identifiant de la chambre dans l'URL
  */
-router.get('/edit', chambreController.editForm);
+router.get('/:id/edit', ChambreController.edit);
 
 /**
  * Route : POST /chambres/:id/edit
  * Description : Met à jour la chambre en base avec les nouvelles infos
  * envoyées depuis le formulaire
  */
-router.post('/edit', chambreController.update);
+router.post('/:id/edit', ChambreController.update);
+
+/**
+ * Route : GET /chambres/:id/delete
+ * Description : Affiche la page de confirmation de suppression
+ */
+router.get('/:id/delete', ChambreController.delete);
 
 /**
  * Route : POST /chambres/:id/delete
  * Description : Supprime une chambre identifiée par son id
  */
-router.post('/delete', chambreController.remove);
+router.post('/:id/delete', ChambreController.destroy);
 
-// On exporte le routeur pour qu’il soit utilisé dans app.js
+// On exporte le routeur pour qu'il soit utilisé dans app.js
 export default router;
--- routes/clients.js
+++ routes/clients.js
@@ -1,49 +1,49 @@
-// Import d’Express pour créer un routeur
-const express = require('express');
+// Import d'Express pour créer un routeur
+import express from 'express';
 const router = express.Router();
 
 // Import du contrôleur qui contient la logique métier
-const clientController = require('../controllers/clientController');
+import clientController from '../controllers/clientController.js';
 
 /**
- * Route : GET /chambres
- * Description : Affiche la liste des chambres
- * et le lien vers le formulaire d’ajout
+ * Route : GET /clients
+ * Description : Affiche la liste des clients
+ * et le lien vers le formulaire d'ajout
  */
 router.get('/', clientController.index);
 
 /**
- * Route : GET /chambres/create
- * Description : Affiche le formulaire de création d’un client
+ * Route : GET /clients/create
+ * Description : Affiche le formulaire de création d'un client
  */
 router.get('/create', clientController.createForm);
 
 /**
- * Route : POST /chambres/create
+ * Route : POST /clients/create
  * Description : Récupère les données envoyées par le formulaire
  * et ajoute un nouveau client en base
  */
-router.post('/:id/create', clientController.create);
+router.post('/create', clientController.create);
 
 /**
- * Route : GET /chambres/:id/edit
- * Description : Affiche le formulaire d’édition d’une chambre existante
- * :id correspond à l’identifiant de la chambre dans l’URL
+ * Route : GET /clients/:id/edit
+ * Description : Affiche le formulaire d'édition d'un client existant
+ * :id correspond à l'identifiant du client dans l'URL
  */
 router.get('/:id/edit', clientController.editForm);
 
 /**
- * Route : POST /chambres/:id/edit
- * Description : Met à jour la chambre en base avec les nouvelles infos
+ * Route : POST /clients/:id/edit
+ * Description : Met à jour le client en base avec les nouvelles infos
  * envoyées depuis le formulaire
  */
 router.post('/:id/edit', clientController.update);
 
 /**
- * Route : POST /chambres/:id/delete
- * Description : Supprime une chambre identifiée par son id
+ * Route : POST /clients/:id/delete
+ * Description : Supprime un client identifié par son id
  */
 router.post('/:id/delete', clientController.remove);
 
-// On exporte le routeur pour qu’il soit utilisé dans app.js
-module.exports = router;
+// On exporte le routeur pour qu'il soit utilisé dans app.js
+export default router;
