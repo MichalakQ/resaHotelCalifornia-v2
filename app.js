1617ac1f5b305b223816d619afa36793718a3ccd
MichalakQ <q.michalak@laposte.net>
2026-01-05 14:36:15 +0100

    set helmet

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Helmet - Sécurité
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrc: ["'self'"],
+        imgSrc: ["'self'", "data:"],
+        fontSrc: ["'self'"],
+      },
+    },
+  })
+);
 // View engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
