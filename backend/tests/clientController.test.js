0fc1293324612919ee54ce6ce8032904b3116895
adam <adam@me.fr>
2026-01-13 10:40:57 +0100

    création test unitaires pour clientController en cours

---
+++ backend/tests/clientController.test.js
@@ -0,0 +1,12 @@
+const ClientController = require('../controllers/clientController');
+const { Client } = require('../models');
+
+const mockClient = {
+    findAll: jest.fn(),
+  };
+
+describe('doit afficher la liste des clients',() => {
+
+
+
+})
